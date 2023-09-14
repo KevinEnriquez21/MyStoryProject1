f881d5765c0ddfc1f5c37ed6faf8cb197059155e
KevinEnriquez21 <118208568+KevinEnriquez21@users.noreply.github.com>
2023-09-14 13:37:48 -0400

    Add files via upload
    latest working

---
+++ MyStory Project/BackstoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  detailViewController.swift
+//  MyStory Project
+//
+//  Created by Kevin 😈 on 9/13/23.
+//
+
+import UIKit
+
+class backstoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MyStory Project/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MyStory Project
+//
+//  Created by Kevin 😈 on 9/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
